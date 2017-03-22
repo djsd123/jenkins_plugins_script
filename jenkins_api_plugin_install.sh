30cca09f9e82609b41a56618a53b036c843b5ed9
Mikael <mikellallison@gmail.com>
2017-03-22 16:30:21 +0000

    Allowing the script to run interactively

--- jenkins_api_plugin_install.sh
+++ jenkins_api_plugin_install.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 # Source plugin Array
 . ./plugins.sh
 
@@ -11,8 +9,27 @@ JENKINS_URL=pluginManager/installNecessaryPlugins
 # API User
 API_USERNAME=$(whoami)
 
-#API User's Password
-API_PASSWORD=$API_USERNAME
+#API user's password
+#API_PASSWORD=$API_USERNAME
+
+#API user's password (interactive)
+echo -n "password: "
+read -s PASSWORD
+API_PASSWORD=$PASSWORD
+
+usage() {
+cat <<EOH
+
+You must supply the password for your user account in Jenkins.
+
+EOH
+
+}
+
+if [[ -z "$API_PASSWORD" ]]; then
+  usage
+  exit 1
+fi
 
 # Get user's api Token
 API_TOKEN=$(curl http://localhost:8080/user/"${API_USERNAME}"/configure?pretty --user "${API_USERNAME}":"${API_PASSWORD}" |grep -o 'value\=\"[a-z0-9]\{32\}\"' | tr -d '"'| sed 's/\<value\>//' | tr -d '=')
@@ -22,6 +39,11 @@ CSRF_CRUMB_HEADER=$(curl -XGET 'http://'"${API_USERNAME}"':'"${API_TOKEN}"'@loca
 
 #Install plugins
 for i in "${PLUGINS[@]}"; do
-  curl -XPOST 'http://'"${API_USERNAME}"':'"${API_TOKEN}"'@'"${HOST}"'/'"${JENKINS_URL}"'' -d '<jenkins><install plugin="'"${i}"'" /></jenkins>' -H ''"${CSRF_CRUMB_HEADER}"''
+  curl --fail --silent -XPOST 'http://'"${API_USERNAME}"':'"${API_TOKEN}"'@'"${HOST}"'/'"${JENKINS_URL}"'' -d '<jenkins><install plugin="'"${i}"'" /></jenkins>' -H ''"${CSRF_CRUMB_HEADER}"''
 done
+
+if [[ $? -eq 22 ]]; then
+  echo "Failed to authenticate.  Possibly a bad password?"
+  exit 1
+fi
 
